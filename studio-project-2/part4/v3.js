6869fe6458c3e12a8ff07914ab2d18cff6b40ba6
Aria Zhou <zhouq743@newschool.edu>
2021-02-23 20:08:57 -0500

    project 2 part 5

---
+++ studio-project-2/part4/v3.js
@@ -0,0 +1,31 @@
+function redc(){
+    document.querySelectorAll('.c').forEach(item => {
+        item.classList.add('test2');
+    });
+}
+function redh(){
+    document.querySelectorAll('.h').forEach(item => {
+        item.classList.add('test2');
+    });
+}
+function redo(){
+    document.querySelectorAll('.o').forEach(item => {
+        item.classList.add('test2');
+    });
+}
+function haha(){
+    document.querySelectorAll('.c').forEach(item => {
+        item.classList.add('test2');
+    });
+    document.querySelectorAll('.h').forEach(item => {
+        item.classList.add('test2');
+    });
+    document.querySelectorAll('.o').forEach(item => {
+        item.classList.add('test2');
+    });
+}
+function nono(){
+    document.querySelectorAll('.test2').forEach(item => {
+        item.classList.remove('test2');
+    });
+}
