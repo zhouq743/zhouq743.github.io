7ccf3f10df1846f8826fc6a95b76d27b1c4d087c
Aria Zhou <zhouq743@newschool.edu>
2021-04-02 19:31:53 -0400

    web assignment airtable

--- lab-2021/web-tour/main.js
+++ lab-2021/web-tour/main.js
@@ -46,8 +46,7 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+  var slides = document.getElementsByClassName("my-slides");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
