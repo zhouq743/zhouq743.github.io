88da4a27f87713c0ab42ba21e8436919fd2fe0d1
zhouq743 <77871194+zhouq743@users.noreply.github.com>
2022-09-30 00:00:36 -0400

    core 4 week4

---
+++ pulp-fiction/index.js
@@ -0,0 +1,26 @@
+$("#start").click(function () {
+  $("#container").fadeOut(1);
+  $("#part1").fadeIn(1);
+  document.querySelector("#part1").play();
+});
+document.querySelector("#part1").addEventListener("ended", function () {
+  document.querySelector("#part1").style.display = "none";
+  $("#later").css("display", "flex");
+});
+$("#option-b").click(function () {
+  $("#later").fadeOut(1);
+  $("#part2").fadeIn(1);
+  document.querySelector("#part2").play();
+});
+$("#option-a").click(function () {
+  $("#later").fadeOut(1);
+  document.querySelector("#part3").play();
+});
+$("#option-c").click(function () {
+  $("#later").fadeOut(1);
+  document.querySelector("#part3").play();
+});
+$("#option-d").click(function () {
+  $("#later").fadeOut(1);
+  document.querySelector("#part3").play();
+});
